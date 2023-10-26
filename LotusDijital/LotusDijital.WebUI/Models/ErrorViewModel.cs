455f31c2c3317e6d6f35926a7f6a7b7c8532d4cc
MuhammetCkr <muhammetckrr@gmail.com>
2023-10-26 22:30:12 +0300

    a

---
+++ LotusDijital/LotusDijital.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LotusDijital.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
